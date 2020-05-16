c90b34c75bff4a7545ec0a8b3286c4c0fe3f20af
svasenkov <aanher@gmail.com>
2020-05-16 15:38:12 +0300

    чистый коммит перед pageobject

--- src/test/java/tests/FacebookTests.java
+++ src/test/java/tests/FacebookTests.java
@@ -1,14 +1,11 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.*;
-import pages.FacebookPage;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static helpers.Environment.*;
@@ -22,34 +19,13 @@ class FacebookTests extends TestBase {
     @Disabled
     @Description("Positive test with testid")
     void successfulLoginWithTestId() {
-        open("http://facebook.com");
+        open(url);
 
-        $(by("data-testid", "royal_email")).setValue("qa.guru.test@gmail.com"); // Do not store private data in code!
-//        $("#email").setValue("qa.guru.test@gmail.com");
-//        $(byId("email")).setValue("qa.guru.test@gmail.com");
-//        $(".inputtext.login_form_input_box").setValue("qa.guru.test@gmail.com");
-//        $(".login_form_input_box").setValue("qa.guru.test@gmail.com");
-
-        $(by("data-testid", "royal_pass")).setValue("testpassword#&!");
+        $(by("data-testid", "royal_email")).setValue(email); // Do not store private data in code!
+        $(by("data-testid", "royal_pass")).setValue(password);
         $(by("data-testid", "royal_login_button")).click();
-//        $(byText("Вход")).click();
 
         $("html").shouldHave(text("Qa"), text("Guru"));
     }
 
-    @Test
-    @Description("Negative test with PageObject, account blocked")
-    void unSuccessfulLoginWithPageObject() {
-//        Configuration.headless = true;
-        FacebookPage facebookPage = new FacebookPage();
-
-        open(url);
-
-        facebookPage.typeEmail(email);
-        facebookPage.typePassword(password);
-        facebookPage.clickSubmit();
-
-        $(byText("Ваш аккаунт отключен")).shouldBe(visible);
-//        facebookPage.verifyLoggedInAsUser("Qa", "Guru");
-    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +12,7 @@ public class TestBase {
 
     @BeforeAll
     public static void setUp() {
+        Configuration.headless = true;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
     }
 
