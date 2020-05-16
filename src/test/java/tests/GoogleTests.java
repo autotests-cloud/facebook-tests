73f5e32fa3e93a2ba6d4486603e9a306cb0166af
svasenkov <aanher@gmail.com>
2020-05-16 16:00:02 +0300

    Чистый коммит перед PageObject

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -5,7 +5,6 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import pages.GooglePage;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
@@ -26,17 +25,5 @@ class GoogleTests extends TestBase {
 
         $("html").shouldHave(text("Лепрозорий: вход"));
     }
-
-    @Test
-    @Description("Positive test with testid with PO")
-    void successfulSearchWithPO() {
-        GooglePage googlePage = new GooglePage();
-
-        open("http://google.com");
-
-        googlePage.typeSearch("lepra");
-
-        googlePage.verifySearchHasResult("Лепрозорий: вход");
-    }
-
+    
 }
